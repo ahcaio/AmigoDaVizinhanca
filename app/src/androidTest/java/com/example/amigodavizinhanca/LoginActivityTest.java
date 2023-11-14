ac822c9e92bc57f6fbf06bc2d36d7a56a789e087
Daniel <danielgondimdecastro@gmail.com>
2023-11-14 20:44:42 -0300

    test: adiciona teste para login realizado com sucesso

--- app/src/androidTest/java/com/example/amigodavizinhanca/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/amigodavizinhanca/LoginActivityTest.java
@@ -6,25 +6,30 @@ import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 public class LoginActivityTest {
 
+    @Rule
+    public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule<>(LoginActivity.class);
+
     @Test
-    void testaLoginBemSucedido() {
+    public void testaLoginBemSucedido() {
         // Enter correct username and password
         Espresso.onView(ViewMatchers.withId(R.id.edtTextEmailAddress))
                 .perform(ViewActions.typeText("admin"));
-        Espresso.onView(ViewMatchers.withId(R.id.edtTextPassword)
-                .perform(ViewActions.typeText("password")));
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextPassword))
+                .perform(ViewActions.typeText("admin123"));
 
         // Click the login button
         Espresso.onView(ViewMatchers.withId(R.id.btnLogin))
                 .perform(ViewActions.click());
 
         // Check if the login success message is displayed
-        Espresso.onView(ViewMatchers.withId(R.id.resultTextView))
+        Espresso.onView(ViewMatchers.withId(R.id.edtTxtResposta))
                 .check(ViewAssertions.matches(withText("Login realizado com sucesso")));
     }
 
