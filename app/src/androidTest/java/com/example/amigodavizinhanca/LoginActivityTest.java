cc14f0b5ed11b8ae37dc7160356149ea7d544c82
Daniel <danielgondimdecastro@gmail.com>
2023-11-27 21:14:07 -0300

    test: corrige testes para realização de login com sucesso

--- app/src/androidTest/java/com/example/amigodavizinhanca/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/amigodavizinhanca/LoginActivityTest.java
@@ -1,10 +1,9 @@
 package com.example.amigodavizinhanca;
 
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
@@ -29,8 +28,62 @@ public class LoginActivityTest {
                 .perform(ViewActions.click());
 
         // Check if the login success message is displayed
-        Espresso.onView(ViewMatchers.withId(R.id.edtTxtResposta))
-                .check(ViewAssertions.matches(withText("Login realizado com sucesso")));
+        Espresso.onView(ViewMatchers.withText("Login realizado com sucesso"))
+                .inRoot(RootMatchers.isDialog())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
+    @Test
+    public void testaLoginComUsuarioCorretoESenhaIncorreta() {
+        // Enter correct username and password
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextEmailAddress))
+                .perform(ViewActions.typeText("admin"));
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextPassword))
+                .perform(ViewActions.typeText("admin"));
+
+        // Click the login button
+        Espresso.onView(ViewMatchers.withId(R.id.btnLogin))
+                .perform(ViewActions.click());
+
+        // Check if the login success message is displayed
+        Espresso.onView(ViewMatchers.withText("A senha está incorreta. Favor tentar novamente."))
+                .inRoot(RootMatchers.isDialog())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void testaLoginComUsuarioIncorretoESenhaCorreta() {
+        // Enter correct username and password
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextEmailAddress))
+                .perform(ViewActions.typeText("adminn"));
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextPassword))
+                .perform(ViewActions.typeText("admin123"));
+
+        // Click the login button
+        Espresso.onView(ViewMatchers.withId(R.id.btnLogin))
+                .perform(ViewActions.click());
+
+        // Check if the login success message is displayed
+        Espresso.onView(ViewMatchers.withText("O usuário está incorreto. Favor tentar novamente com outro usuário."))
+                .inRoot(RootMatchers.isDialog())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void testaLoginComUsuarioESenhaIncorreta() {
+        // Enter correct username and password
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextEmailAddress))
+                .perform(ViewActions.typeText("adminN"));
+        Espresso.onView(ViewMatchers.withId(R.id.edtTextPassword))
+                .perform(ViewActions.typeText("admin"));
+
+        // Click the login button
+        Espresso.onView(ViewMatchers.withId(R.id.btnLogin))
+                .perform(ViewActions.click());
+
+        // Check if the login success message is displayed
+        Espresso.onView(ViewMatchers.withText("A senha e o usuário estão incorretos. Favor tentar novamente."))
+                .inRoot(RootMatchers.isDialog())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
 }
