398e47ea00603a3097f45192dde0de6f26a9da07
Daniel <danielgondimdecastro@gmail.com>
2023-11-27 21:11:34 -0300

    feat: instancia firebase para realizar autenticação de usuário

--- app/src/main/java/com/example/amigodavizinhanca/LoginActivity.java
+++ app/src/main/java/com/example/amigodavizinhanca/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.amigodavizinhanca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +16,8 @@ import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // instancia o Firebase
+    private FirebaseAuth mAuth;
     EditText edtTextEmailAddress;
     EditText edtTextPassword;
     Button btnLogin;
@@ -24,6 +30,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Inicializa o FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
         edtTextEmailAddress = findViewById(R.id.edtTextEmailAddress);
         edtTextPassword = findViewById(R.id.edtTextPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -36,14 +46,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         String email = edtTextEmailAddress.getText().toString();
         String password = edtTextPassword.getText().toString();
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Login");
+
         if (Objects.equals(usuarioTeste, email) && Objects.equals(passwordTeste, password)) {
-            txtView.setText("Login realizado com sucesso");
+            builder.setMessage("Login realizado com sucesso");
         } else if (!Objects.equals(usuarioTeste, email) && Objects.equals(passwordTeste, password)) {
-            txtView.setText("O usuário está incorreto. Favor tentar novamente com outro usuário.");
+            builder.setMessage("O usuário está incorreto. Favor tentar novamente com outro usuário.");
         } else if (Objects.equals(usuarioTeste, email) && !Objects.equals(passwordTeste, password)) {
-            txtView.setText("A senha está incorreta. Favor tentar novamente.");
+            builder.setMessage("A senha está incorreta. Favor tentar novamente.");
         } else {
-            txtView.setText("A senha e o usuário estão incorretos. Favor tentar novamente.");
+            builder.setMessage("A senha e o usuário estão incorretos. Favor tentar novamente.");
         }
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Código a ser executado quando o usuário clica em OK
+                dialog.dismiss(); // Fechar o diálogo
+            }
+        });
+
+        builder.create().show();
     }
 }
