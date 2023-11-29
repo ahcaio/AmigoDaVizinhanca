ef0407606ee030d3e11a4f984b950bf4dc0ecdc9
Daniel <danielgondimdecastro@gmail.com>
2023-11-29 19:42:10 -0300

    feat: adiciona a dependência do FirebaseUi ao build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,8 @@ android {
 }
 
 dependencies {
-
+//    implementation("com.google.firebase:firebase-auth:21.0.1")
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
