b72ed7ff93af5a4dfdccc4683c94b403dec59f4b
Daniel <danielgondimdecastro@gmail.com>
2023-11-29 20:53:26 -0300

    fix: altera a lógica da classe para implementar o FirebaseUI

--- app/src/main/java/com/example/amigodavizinhanca/LoginActivity.java
+++ app/src/main/java/com/example/amigodavizinhanca/LoginActivity.java
@@ -1,40 +1,47 @@
 package com.example.amigodavizinhanca;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    // instancia o Firebase para autenticar o usuário
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     EditText edtTextEmailAddress;
     EditText edtTextPassword;
     Button btnLogin;
     TextView txtView;
 
-//    private String usuarioTeste = "admin";
-//    private String passwordTeste = "admin123";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        //inicializa a instância do firebaseauth
         mAuth = FirebaseAuth.getInstance();
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
 
         edtTextEmailAddress = findViewById(R.id.edtTextEmailAddress);
         edtTextPassword = findViewById(R.id.edtTextPassword);
@@ -45,43 +52,52 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-        String email = edtTextEmailAddress.getText().toString();
-        String password = edtTextPassword.getText().toString();
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()))
+                .build();
 
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setTitle("Login");
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(getApplicationContext(), "Login realizado com sucesso", Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Falha no login", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+        signInLauncher.launch(signInIntent);
     }
-//
-//        if (Objects.equals(usuarioTeste, email) && Objects.equals(passwordTeste, password)) {
-//            builder.setMessage("Login realizado com sucesso");
-//        } else if (!Objects.equals(usuarioTeste, email) && Objects.equals(passwordTeste, password)) {
-//            builder.setMessage("O usuário está incorreto. Favor tentar novamente com outro usuário.");
-//        } else if (Objects.equals(usuarioTeste, email) && !Objects.equals(passwordTeste, password)) {
-//            builder.setMessage("A senha está incorreta. Favor tentar novamente.");
-//        } else {
-//            builder.setMessage("A senha e o usuário estão incorretos. Favor tentar novamente.");
-//        }
-//
-//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int id) {
-//                // Código a ser executado quando o usuário clica em OK
-//                dialog.dismiss(); // Fechar o diálogo
-//            }
-//        });
-//
-//        builder.create().show();
+
+//        String email = edtTextEmailAddress.getText().toString();
+//        String password = edtTextPassword.getText().toString();
+
+
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            Toast.makeText(getApplicationContext(), "Login realizado com sucesso", Toast.LENGTH_LONG).show();
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), "Falha no login", Toast.LENGTH_LONG).show();
+//                        }
+//                    }
+//                });
 //    }
+
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Toast.makeText(getApplicationContext(), "Login realizado com sucesso", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Falha no login", Toast.LENGTH_LONG).show();
+        }
+    }
 }
