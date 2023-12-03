58b22b6116dc4c43a04eb6d832429a1fb8df6af7
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-12-03 16:04:50 -0300

    fix: deleta o método criaComp() para que a inicialização dos componentes seja feit no método onCreate()

--- app/src/main/java/com/example/amigodavizinhanca/CadastroDeServico.java
+++ app/src/main/java/com/example/amigodavizinhanca/CadastroDeServico.java
@@ -2,6 +2,7 @@ package com.example.amigodavizinhanca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.style.MaskFilterSpan;
 import android.view.View;
@@ -23,72 +24,65 @@ public class CadastroDeServico extends AppCompatActivity implements View.OnClick
     String acao;
     Vaga vaga;
 
-    public void criarComp() {
-        btnCadastrarVagas = findViewById(R.id.btnCadastrar);
-        btnCadastrarVagas.setOnClickListener(this);
-        btnCadastrarVagas.setText(acao);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_de_servico);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            if (extras.containsKey("acao")) {
-                acao = extras.getString("acao");
-            }
-            dao = new VagaDao(this);
-            criarComp();
 
-            if (extras.getSerializable("obj") != null) {
-                vaga = (Vaga) extras.getSerializable("obj");
-                edtNomeVaga.setText(vaga.getNome());
-                edtEndereco.setText(vaga.getEndereco());
-                edtTelefone.setText(vaga.getTelefone());
-                edtEmail.setText(vaga.getEmail());
-                edtDescricao.setText(vaga.getDescricao());
+        btnCadastrarVagas = findViewById(R.id.btnCadastrar);
+        btnCadastrarVagas.setOnClickListener(this);
+        edtNomeVaga = findViewById(R.id.edtNomeVaga);
+        edtEndereco = findViewById(R.id.edtEndereco);
+        edtTelefone = findViewById(R.id.edtFone);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtDescricao = findViewById(R.id.edtDescricao);
+        Intent intent = getIntent();
+        if (intent.hasExtra("obj")) {
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                if (extras.containsKey("acao")) {
+                    acao = extras.getString("acao");
+                }
+
+                if (extras.getSerializable("obj") != null) {
+                    vaga = (Vaga) extras.getSerializable("obj");
+                    assert vaga != null;
+                    edtNomeVaga.setText(vaga.getNome());
+                    edtEndereco.setText(vaga.getEndereco());
+                    edtTelefone.setText(vaga.getTelefone());
+                    edtEmail.setText(vaga.getEmail());
+                    edtDescricao.setText(vaga.getDescricao());
+                }
+                dao = new VagaDao(this);
             }
+
         } else {
             // Handle the case when extras are null
             // You may want to show an error message or finish the activity
             Toast.makeText(this, "Extras are null", Toast.LENGTH_SHORT).show();
             finish(); // Finish the activity
         }
-//        acao = Objects.requireNonNull(Objects.requireNonNull(getIntent().getExtras()).getString("acao"));
-//        dao = new VagaDao(this);
-//        criarComp();
-//
-//        if (getIntent().getExtras() != null && getIntent().getExtras().getSerializable("obj") != null) {
-//            vaga = (Vaga) getIntent().getExtras().getSerializable("obj");
-//            assert vaga != null;
-//            edtNomeVaga.setText(vaga.getNome());
-//            edtEndereco.setText(vaga.getEndereco());
-//            edtTelefone.setText(vaga.getTelefone());
-//            edtEmail.setText(vaga.getEmail());
-//            edtDescricao.setText(vaga.getDescricao());
-//        } else {
-//            // Handle the case when extras are null
-//            // You may want to show an error message or finish the activity
-//            Toast.makeText(this, "Extras are null",
-//                    Toast.LENGTH_SHORT).show();
-//            finish(); // Finish the activity
-//        }
-
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnCadastrarVagas) {
-            vaga.setNome(edtNomeVaga.getText().toString());
-            vaga.setEndereco(edtEndereco.getText().toString());
-            vaga.setTelefone(edtTelefone.getText().toString());
-            vaga.setEmail(edtEmail.getText().toString());
-            vaga.setDescricao(edtDescricao.getText().toString());
-        }
-        if (acao.equals("Inserir")) {
-            long id = dao.inserir(vaga);
-            Toast.makeText(this, "Vaga" + vaga.getNome() + " foi inserido com o id = " + id, Toast.LENGTH_LONG).show();
+            if (vaga != null && edtNomeVaga != null && dao != null && acao != null) {
+                vaga.setNome(edtNomeVaga.getText().toString());
+                vaga.setEndereco(edtEndereco.getText().toString());
+                vaga.setTelefone(edtTelefone.getText().toString());
+                vaga.setEmail(edtEmail.getText().toString());
+                vaga.setDescricao(edtDescricao.getText().toString());
+            }
+            assert acao != null;
+            if (acao.equals("Inserir")) {
+                assert dao != null;
+                long id = dao.inserir(vaga);
+                Toast.makeText(this, "Vaga " + vaga.getNome() + " foi inserido com o id = " + id, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // Tratar o caso quando vaga, edtNomeVaga, dao ou acao são nulos
+            Toast.makeText(this, "Alguma variável é nula", Toast.LENGTH_SHORT).show();
         }
     }
 }
