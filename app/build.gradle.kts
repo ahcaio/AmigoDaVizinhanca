cbe1e1833cd0f007924ea1451199fb0a9952df07
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-12-07 17:21:15 -0300

    fix: adiciona dependências do espresso e do firebaseui para testes de login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.amigodavizinhanca"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.amigodavizinhanca"
@@ -30,16 +30,25 @@ android {
 }
 
 dependencies {
-//    implementation("com.google.firebase:firebase-auth:21.0.1")
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //noinspection GradleCompatible
     implementation("com.android.support:cardview-v7:28.0.0")
-    androidTestImplementation("androidx.test:rules:1.4.0")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    // Dependências do Firebase Testing e FirebaseUI
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-web:3.5.1")
+    androidTestImplementation("androidx.test.espresso.idling:idling-concurrent:3.5.1")
+    androidTestImplementation("androidx.test.espresso.idling:idling-net:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
+    androidTestImplementation("com.firebaseui:firebase-ui-auth:8.0.2")
 }
