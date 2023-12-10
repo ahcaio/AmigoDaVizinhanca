63fdd5df14cba17763a237ea9ccb4ed98ab1ec91
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-12-10 18:25:13 -0300

    fix: corrige SQL que cria a tabela cadastro_pj e suas colunas

--- app/src/main/java/bancodedados/Conexao.java
+++ app/src/main/java/bancodedados/Conexao.java
@@ -14,13 +14,18 @@ public class Conexao extends SQLiteOpenHelper {
     // Versão do banco de dados
     private static final int DATABASE_VERSION = 1;
 
-    private static final String SQL_CREATE_VAGA = "create table vagas ( "
+    private static final String SQL_CREATE_VAGA = "create table cadastro_pj ( "
             + "id integer PRIMARY KEY AUTOINCREMENT,"
-            + "nome VARCHAR(100), "
-            + "endereco VARCHAR(200), "
-            + "telefone VARCHAR(20), "
-            + "email VARCHAR(50), "
+            + "razao_social VARCHAR(100), "
+            + "cnpj VARCHAR(200), "
+            + "cep VARCHAR(20), "
+            + "logradouro VARCHAR(50), "
+            + "localidade VARCHAR(50), "
+            + "uf VARCHAR(50), "
+            + "numero VARCHAR(50), "
+            + "complemento VARCHAR(50), "
             + "descricao text );";
+
 
     // Construtor
     public Conexao(@Nullable Context context) {
@@ -30,11 +35,6 @@ public class Conexao extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_VAGA);
-//        Cursor cursor = db.rawQuery("SELECT sql FROM sqlite_master WHERE name='vagas'", null);
-//        if (cursor.moveToFirst()) {
-//            Log.d("Database", "Create Table Query: " + cursor.getString(0));
-//        }
-//        cursor.close();
     }
 
     @Override
