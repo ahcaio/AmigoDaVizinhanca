113ef345f841321d7b8c9426c58269304b33bdb6
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-12-10 22:55:12 -0300

    test: adiciona método testaExclusaoPessoaJuridica

--- app/src/androidTest/java/com/example/amigodavizinhanca/CadastroPjDaoTest.java
+++ app/src/androidTest/java/com/example/amigodavizinhanca/CadastroPjDaoTest.java
@@ -17,18 +17,13 @@ import model.CadastroPJ;
 public class CadastroPjDaoTest extends TestCase {
 
     private CadastroPjDao cadastroPjDao;
+    CadastroPJ cadastroPJTeste = new CadastroPJ();
 
     @Before
     public void setUp() {
         Context context = ApplicationProvider.getApplicationContext();
         cadastroPjDao = new CadastroPjDao(context);
-    }
 
-    @Test
-    public void testaInserirPessoaJuridica() {
-
-        // inicializa o objeto
-        CadastroPJ cadastroPJTeste = new CadastroPJ();
         cadastroPJTeste.setRazaoSocial("Instituição 1");
         cadastroPJTeste.setCpnj("1223178237287312");
         cadastroPJTeste.setEmail("admin@gmail.com");
@@ -38,6 +33,22 @@ public class CadastroPjDaoTest extends TestCase {
         cadastroPJTeste.setUf("Goiás");
         cadastroPJTeste.setNumero("1");
         cadastroPJTeste.setComplemento("Casa 2");
+    }
+
+    @Test
+    public void testaInserirPessoaJuridica() {
+
+        // inicializa o objeto
+//        CadastroPJ cadastroPJTeste = new CadastroPJ();
+//        cadastroPJTeste.setRazaoSocial("Instituição 1");
+//        cadastroPJTeste.setCpnj("1223178237287312");
+//        cadastroPJTeste.setEmail("admin@gmail.com");
+//        cadastroPJTeste.setCep("74290-045");
+//        cadastroPJTeste.setLogradouro("Avenida teste");
+//        cadastroPJTeste.setLocalidade("Goiânia");
+//        cadastroPJTeste.setUf("Goiás");
+//        cadastroPJTeste.setNumero("1");
+//        cadastroPJTeste.setComplemento("Casa 2");
 
         long id = cadastroPjDao.inserir(cadastroPJTeste);
 
@@ -59,6 +70,25 @@ public class CadastroPjDaoTest extends TestCase {
         assert cadastroPjDoBanco.getEmail().equals("admin@gmail.com");
         assert cadastroPjDoBanco.getCep().equals("74290-045");
 //        assert cadastroPjDoBanco.getCep().equals("74290-045");
+    }
+
+    @Test
+    public void testaExcluirPessoaJuridica() {
+        // inicializa o objeto
+        CadastroPJ cadastroPJTeste2 = new CadastroPJ();
+        cadastroPJTeste.setRazaoSocial("Instituição 1");
+        cadastroPJTeste.setCpnj("1223178237287312");
+        cadastroPJTeste.setEmail("admin@gmail.com");
+        cadastroPJTeste.setCep("74290-045");
+        cadastroPJTeste.setLogradouro("Avenida teste");
+        cadastroPJTeste.setLocalidade("Goiânia");
+        cadastroPJTeste.setUf("Goiás");
+        cadastroPJTeste.setNumero("1");
+        cadastroPJTeste.setComplemento("Casa 2");
+        cadastroPjDao.inserir(cadastroPJTeste);
+        cadastroPjDao.excluir(cadastroPJTeste);
+        List<CadastroPJ> empresasNoBanco = cadastroPjDao.listar();
+        assertNull(empresasNoBanco);
     }
 }
 
