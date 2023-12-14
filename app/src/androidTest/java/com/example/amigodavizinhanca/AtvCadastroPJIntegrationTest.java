26273e87bba649fb5f396390e7e713ba8994d277
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-12-13 23:12:01 -0300

    test: define manualmente os extras para executar a ação de cadastro

--- app/src/androidTest/java/com/example/amigodavizinhanca/AtvCadastroPJIntegrationTest.java
+++ app/src/androidTest/java/com/example/amigodavizinhanca/AtvCadastroPJIntegrationTest.java
@@ -5,9 +5,14 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
-import static org.hamcrest.CoreMatchers.allOf;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import junit.framework.TestCase;
@@ -24,23 +29,6 @@ public class AtvCadastroPJIntegrationTest extends TestCase {
     public ActivityScenarioRule<AtvCadastroPJ> activityScenarioRule = new ActivityScenarioRule<AtvCadastroPJ>(AtvCadastroPJ.class);
 
     @Test
-    public void testaPreencherCamposDoCadastroPJEClicarNoBotaoCadastrar() {
-        // Etapa 1: Simular o cadastro de uma empresa PJ
-        onView(withId(R.id.edtNomeVaga)).perform(replaceText("Instituição teste"));
-        onView(withId(R.id.edtEmail)).perform(typeText("teste@gmail.com"));
-        onView(withId(R.id.edtCNPJ)).perform(typeText("1234567890-0001/00"));
-        onView(withId(R.id.edtCEP)).perform(typeText("74290-045"));
-        onView(withId(R.id.edtLogradouro)).perform(typeText("Avenida teste"));
-        onView(withId(R.id.edtLocalidade)).perform(replaceText("Goiânia"));
-        onView(withId(R.id.edtUF)).perform(replaceText("Goiás"));
-        onView(withId(R.id.edtNumero)).perform(typeText("1000"));
-        onView(withId(R.id.edtComplemento)).perform(typeText("Casa 2"));
-
-        onView(withId(R.id.btnCadastrarPj)).perform(click());
-
-    }
-
-    @Test
     public void testaCadastroDePessoaJuridica() {
         // Etapa 1: Simular o cadastro de uma empresa PJ
         onView(withId(R.id.edtNomeVaga)).perform(replaceText("Instituição teste"));
@@ -52,6 +40,11 @@ public class AtvCadastroPJIntegrationTest extends TestCase {
         onView(withId(R.id.edtUF)).perform(replaceText("Goiás"));
         onView(withId(R.id.edtNumero)).perform(typeText("1000"));
         onView(withId(R.id.edtComplemento)).perform(typeText("Casa 2"));
+
+        Intent intent = new Intent();
+        intent.putExtra("acao", "inserir");
+        Intents.intending(hasComponent(AtvCadastroPJ.class.getName()))
+                .respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, intent));
 
         onView(withId(R.id.btnCadastrarPj)).perform(click());
     }
